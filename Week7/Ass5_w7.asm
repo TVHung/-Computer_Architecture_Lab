d72bdfa65c6bf0bc52d93e19791c4eb299c7f894
truong hung <truonghungvatvo@gmail.com>
2020-04-21 23:52:01 +0700

    update week7

---
+++ Week7/Ass5_w7.asm
@@ -0,0 +1,15 @@
+.data
+	Largest:	.asciiz "Largest: "
+	Smallest:	.asciiz "Smallest: "
+	phay:		.asciiz ", "
+.text
+	li	$s0, 1
+	li	$s1, 12
+	li	$s2, 9
+	li	$s3, 2
+	li	$s4, 0
+	li	$s5, 8
+	li	$s6, 2
+	li	$s7, 5
+Main:	
+	
